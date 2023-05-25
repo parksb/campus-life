5e6c137953842f03134748df10b4468219a47469
parksb <parkgds@gmail.com>
2023-05-25 21:34:00 +0900

    영처: wiener filtering 개선

--- dgmd341-digital-image-processing/assignments/wiener-filtering/main.cpp
+++ dgmd341-digital-image-processing/assignments/wiener-filtering/main.cpp
@@ -3,38 +3,7 @@
 
 using namespace cv;
 
-Mat wiener_filter_manual(const Mat& src, const Mat& ker, const int k) {
-    Mat srcf, kerf, retf, ret;
-
-    dft(src, srcf, DFT_COMPLEX_OUTPUT);
-    dft(ker, kerf, DFT_COMPLEX_OUTPUT);
-
-    retf.create(srcf.size(), srcf.type());
-
-    for (Point p(0, 0); p.y < retf.rows; p.y++) {
-        for (p.x = 0; p.x < retf.cols; p.x++) {
-            float srcr = srcf.at<Vec2f>(p)[0];
-            float srci = srcf.at<Vec2f>(p)[1];
-            float kerr = kerf.at<Vec2f>(p)[0];
-            float keri = kerf.at<Vec2f>(p)[1];
-
-            float g = srcr + srci;
-            float h = kerr + keri;
-            float m = kerr * kerr + keri * keri;
-
-            retf.at<Vec2f>(p) = Vec2f(
-                kerr * srcr + keri * srci,
-                ((kerr - keri) / m) * (m / (m + k)) * g
-            ) / m;
-        }
-    }
-
-    dft(retf, ret, DFT_INVERSE | DFT_SCALE | DFT_REAL_OUTPUT);
-
-    return ret;
-}
-
-Mat wiener_filter_auto(const Mat& src, const Mat& ker, const int k) {
+Mat wiener_filter(const Mat& src, const Mat& ker, const float k) {
     Mat srcf, kerf;
 
     dft(src, srcf, DFT_COMPLEX_OUTPUT);
@@ -44,11 +13,16 @@ Mat wiener_filter_auto(const Mat& src, const Mat& ker, const int k) {
     Mat ones = Mat::ones(srcf.size(), srcf.type());
     divSpectrums(ones, kerf, a, DFT_ROWS);
 
-    Mat mag;
-    magnitude(kerf, kerf, mag);
+    Mat kerfc[2], mag, h2;
+    split(kerf, kerfc);
+    magnitude(kerfc[0], kerfc[1], mag);
+    multiply(mag, mag, h2);
+
+    Mat zeros = Mat::zeros(h2.size(), h2.type());
+    std::vector<Mat> h2c = { h2, zeros };
+    merge(h2c, h2);
 
     Mat b;
-    Mat h2 = mag * mag;
     divSpectrums(h2, h2 + k, b, DFT_ROWS);
 
     Mat c, retf;
@@ -63,16 +37,16 @@ Mat wiener_filter_auto(const Mat& src, const Mat& ker, const int k) {
 
 int main() {
     Mat input1 = imread("images/input1.png", 0);
-    input1.convertTo(input1, CV_32F, 1 / 255.f);
+    input1.convertTo(input1, CV_32FC1, 1 / 255.f);
 
     Mat input2 = imread("images/input2.png", 0);
-    input2.convertTo(input2, CV_32F, 1 / 255.f);
+    input2.convertTo(input2, CV_32FC1, 1 / 255.f);
 
     Mat kernel = imread("images/kernel.png", 0);
-    kernel.convertTo(kernel, CV_32F, 1 / sum(kernel)[0]);
+    kernel.convertTo(kernel, CV_32FC1, 1 / sum(kernel)[0]);
 
-    Mat filtered1 = wiener_filter_auto(input1, kernel, 2);
-    Mat filtered2 = wiener_filter_auto(input2, kernel, 7);
+    Mat filtered1 = wiener_filter(input1, kernel, 0.0005f);
+    Mat filtered2 = wiener_filter(input2, kernel, 0.002f);
 
     imshow("Input 1", input1);
     imshow("Input 2", input2);
